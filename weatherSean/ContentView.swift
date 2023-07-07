b0323e77d9723bb094d7865f1d92386695dce194
alirast <xdabnormal@gmail.com>
2023-07-07 17:33:52 +0300

    first commit (5th video)

---
+++ weatherSean/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  weatherSean
+//
+//  Created by N S on 07.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherSean/weatherSeanApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherSeanApp.swift
+//  weatherSean
+//
+//  Created by N S on 07.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherSeanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
